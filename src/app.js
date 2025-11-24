33da4641fbd04d98c3f9a54009655a09d5324b7b
veronica860108 <veroruiba@gmail.com>
2025-11-24 12:09:32 +0000

    generadorDominio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import "bootstrap";
-import "./style.css";
+const pronoun = ['the', 'our'];
+const adj = ['great', 'big'];
+const noun = ['jogger', 'racoon'];
+const extenxions = [`.com`, `.net`, `.us`, `.io`]
+function generadorDominios(pronoun, adj, noun,extenxions) {
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; extenxions.length>l; l++) {
+          console.log(`${pronoun[i]}${adj[j]}${noun[k]}${extenxions[l]}`);
+        }
+      }
+    }
+  }
+ 
+}generadorDominios(pronoun,adj,noun,extenxions);
